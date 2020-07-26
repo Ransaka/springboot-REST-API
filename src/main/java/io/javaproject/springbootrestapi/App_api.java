ae234cac205730b3e93c7a170c072c0016a6354d
ransaka <ransakaravihara@gmail.com>
2020-07-26 17:47:35 +0530

    First commit

---
+++ src/main/java/io/javaproject/springbootrestapi/App_api.java
@@ -0,0 +1,14 @@
+package io.javaproject.springbootrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App_api {
+
+	public static void main(String[] args) {
+		SpringApplication.run(App_api.class, args);
+		
+	}
+
+}
